aa87b02dc4c09789fc5d42c94fec67c9db2a569b
Jens Hilligsøe <jens@hilli.dk>
2025-02-18 10:58:56 +0100

    Enable force ascii, implement image conversion to palletted image for Sixel support, which is now there

--- cmd/icat/icat.go
+++ cmd/icat/icat.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/url"
 	"os"
 
@@ -8,10 +10,23 @@ import (
 )
 
 func main() {
+	// Define the -ascii flag
+	forceASCII := flag.Bool("ascii", false, "Force ASCII art output")
 
-	args := os.Args[1:]
+	// Define a custom usage function
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] <image-url-or-path>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	// Get the remaining arguments after flag parsing
+	args := flag.Args()
 
 	if len(args) == 0 {
+		flag.Usage()
 		os.Exit(0)
 	}
 
@@ -20,9 +35,9 @@ func main() {
 
 		url, err := url.Parse(arg)
 		if err == nil && (url.Scheme == "http" || url.Scheme == "https") {
-			err = icat.PrintImageURL(arg)
+			err = icat.PrintImageURL(arg, *forceASCII)
 		} else {
-			err = icat.PrintImageFile(arg)
+			err = icat.PrintImageFile(arg, *forceASCII)
 		}
 		if err != nil {
 			os.Stderr.WriteString(err.Error())
